12faa5e07e11605131834a3fa585ba91851a8b85
rodking <alldnnwanglu@users.noreply.github.com>
2017-06-28 11:01:47 +0800

    - NEW   超出long 类型处理能力的长整型乘法！

---
+++ src/main/com/swx/utils/BigMultiplication.java
@@ -0,0 +1,49 @@
+package com.swx.utils;
+
+/**
+ * @author rodking
+ * 超出long * long 乘法运算器
+ */
+public class BigMultiplication {
+	
+	public static String multiply(String one,String two)
+	{
+		int[] ones,twos;
+		
+		ones = _Str2Ints(one);
+		twos = _Str2Ints(two);
+		
+		int[] overNums = new int[ones.length + twos.length];
+		
+		for(int i=0;i<ones.length;i++){
+			for(int j=0;j<twos.length;j++){
+				overNums[i+j] += ones[i] * twos[j];
+			}
+		}
+		
+		
+		String tempStr ="";
+		for(int i = overNums.length -2;i>=0;i--){
+			overNums[i] = overNums[i] + overNums[i + 1] / 10;
+		}
+		
+		for(int i = 0;i<overNums.length-1;i++){
+			tempStr += overNums[i]%10; 
+		}
+		
+		
+		System.out.println(one +" * " + two + " = "+ (Long.parseLong(one) * Long.parseLong(two)));
+		System.out.println(one +" * " + two + " = "+ tempStr);
+	
+		return tempStr;
+	}
+	
+	private static int[] _Str2Ints(String str){
+		int[] arrays = new int[str.length()];
+		
+		for(int i=0;i<str.length();i++){
+			arrays[i] = Integer.parseInt(str.substring(i, i+1));
+		}
+		return arrays;
+	}
+}
---
+++ src/test/com/swx/utils/BigMutiplicationTest.java
@@ -0,0 +1,9 @@
+package com.swx.utils;
+
+public class BigMutiplicationTest {
+	
+	public static void main(String[] args)
+	{
+		System.out.println(BigMultiplication.multiply("11","11"));
+	}
+}
